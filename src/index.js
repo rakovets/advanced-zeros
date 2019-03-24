ee7c4712e8212f96c5266e0491c05f956263bd18
Dmitry Rakovets <dmitryrakovets@gmail.com>
2019-03-24 23:49:50 +0300

    feat: impl advanced zeros

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  let numberSystem = base;
+  let result = null;
+
+  for (let divider = 2; divider <= base; divider++) {
+    if (numberSystem % divider === 0) {
+      let numberFactorial = number;
+      let countMultiplierFactorial = 0;
+      while (numberFactorial / divider > 0) {
+        countMultiplierFactorial = countMultiplierFactorial + Math.floor(numberFactorial / divider);
+        numberFactorial = Math.floor(numberFactorial / divider);
+      }
+
+      let countMultiplierNumberSystem = 0;
+      while (numberSystem % divider === 0) {
+        numberSystem = Math.floor(numberSystem / divider);
+        countMultiplierNumberSystem++;
+      }
+
+      if (result === null || Math.floor(countMultiplierFactorial / countMultiplierNumberSystem) < result) {
+        result = Math.floor(countMultiplierFactorial / countMultiplierNumberSystem);
+      }
+    }
+  }
+  return result;
 }
